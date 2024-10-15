8ad6cd99780c4875fe4f89c979e053d7075bb9ae
Oliver Grimm <olivergrimm89@gmail.com>
2024-10-15 09:12:20 +0200

    CharacterController Basics

---
+++ Assets/Scripts/PlayerMovementStats.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PlayerMovementStats")]
+public class PlayerMovementStats : ScriptableObject
+{
+    [Header("Walk")] 
+    [Range(1f, 100f)] public float MaxWalkSpeed = 12.5f;
+    [Range(0.25f, 50f)] public float GroundAcceleration = 5f;
+    [Range(0.25f, 50f)] public float GroundDeceleration = 20f;
+    [Range(0.25f, 50f)] public float AirAcceleration = 5f;
+    [Range(0.25f, 50f)] public float AirDeleceration = 5f;
+    
+    [Header("Run")]
+    [Range(1f, 100f)] public float MaxRunSpeed = 20f;
+    
+    [Header("Grounded/Collision Checks")]
+    public LayerMask GroundLayer;
+    public float GroundDetectionRayLength = 0.02f;
+    public float HeadDetectionRayLength = 0.02f;
+    [Range(0f, 1f)] public float HeadWidth = 0.75f;
+    
+}
