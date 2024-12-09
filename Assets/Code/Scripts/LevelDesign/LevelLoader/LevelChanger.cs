2d390d52a30115a45a76718d1b08545caaa29d2b
Yandoro <kurogawaproductions@gmail.com>
2024-12-09 14:23:33 +0100

    DarkMode and OdinStuff

--- Assets/Code/Scripts/LevelDesign/LevelLoader/LevelChanger.cs
+++ Assets/Code/Scripts/LevelDesign/LevelLoader/LevelChanger.cs
@@ -4,4 +4,7 @@
     public class LevelChanger : MonoBehaviour
     {
         [SerializeField] private LevelConnection _connection;
+
+        [SerializeField] private string _targetSceneName;
+        [SerializeField] private Transform _spawnPoint;
     }
