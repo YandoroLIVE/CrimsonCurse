c38f136c9173c6936e28eb8949bc8f419aeba7cf
Oliver Grimm <olivergrimm89@gmail.com>
2025-01-02 15:51:50 +0100

    LevelsInScenemanager + Trigger fix

--- Assets/Code/Scripts/LevelDesign/LevelLoader/LevelChanger.cs
+++ Assets/Code/Scripts/LevelDesign/LevelLoader/LevelChanger.cs
@@ -1,32 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-    using UnityEngine;
-    using UnityEngine.SceneManagement;
-
-    namespace HeroController
+namespace HeroController
+{
+    public class LevelChanger : MonoBehaviour
     {
-        public class LevelChanger : MonoBehaviour
+        [SerializeField] private LevelConnection _connection;
+        [SerializeField] private string _targetSceneName;
+        [SerializeField] private Transform _spawnPoint;
+
+        private void Start()
         {
-            [SerializeField] private LevelConnection _connection;
-            [SerializeField] private string _targetSceneName;
-            [SerializeField] private Transform _spawnPoint;
-
-            private void Start()
+            if (_connection == LevelConnection.ActiveConnection)
             {
-                if (_connection == LevelConnection.ActiveConnection)
-                {
-                    FindObjectOfType<PlayerController>().transform.position = _spawnPoint.position;
-                }
+                FindObjectOfType<PlayerController>().transform.position = _spawnPoint.position;
             }
-            private void OnCollisionEnter2D(Collision2D other)
-            {
-                var player = other.collider.GetComponent<PlayerController>();
-                if (player != null)
-                {
-                    LevelConnection.ActiveConnection = _connection;
-                    SceneManager.LoadScene(_targetSceneName);
-                }
+        }
 
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var player = other.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                Debug.Log("Player entered trigger");
+                LevelConnection.ActiveConnection = _connection;
+                SceneManager.LoadScene(_targetSceneName);
+            }
+            else
+            {
+                Debug.Log("Non-player object entered trigger");
             }
         }
     }
-    
+}
