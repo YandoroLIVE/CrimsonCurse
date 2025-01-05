d8e963a4abaa65fec081d200a9f69db3431fd92e
NeverMei <boger.ina@web.de>
2025-01-05 15:04:37 +0100

    +| added Atmophär VFX to Levels, edit Boss Arena

--- Assets/Code/Scripts/New Folder/Scripts/Ground Enemy/S_PlayerHealth.cs
+++ Assets/Code/Scripts/New Folder/Scripts/Ground Enemy/S_PlayerHealth.cs
@@ -1,6 +1,4 @@
-using Mono.Cecil;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement; // Importiere das SceneManagement-Namespace
 
