8f3a5bbd160aaf43ce08f85beb678cf950993a76
MrPowerpoint <tiziand4404@gmail.com>
2025-01-09 15:32:19 +0100

    Sneaker polishing and hit feedback

--- Assets/Code/Scripts/Gameplay/Enemies/SneakerEnemy.cs
+++ Assets/Code/Scripts/Gameplay/Enemies/SneakerEnemy.cs
@@ -1,11 +1,17 @@
 
+using System.Collections;
 using UnityEngine;
 
 public class SneakerEnemy : BaseEnemy
 {
     private const float POINTMERCYAREA = 0.25f;
+    private const float ATTACKANIMATION_HIT_OFFSET = 0.5f;
     [SerializeField] private EnemyPurify purifyHandler;
     [SerializeField] private float hitRange;
+    [SerializeField] private float contactRange = 1f;
+    [SerializeField] private float contactDamage;
+    [SerializeField] private float contactCooldown = 0.25f;
+    private float contactTimer = 0;
     [SerializeField] private float damage;
     [SerializeField] private int wanderRange;
     [SerializeField] private float attackCooldown;
@@ -19,6 +25,7 @@ public class SneakerEnemy : BaseEnemy
     private float idleTimer;
     Rigidbody2D rigi;
     (Transform transform, Rigidbody2D rigidbody2D, S_PlayerHealth health) _Player = (null, null, null);
+    private Animator animator;
     Vector3 currentTargetPoint = Vector3.zero;
     Vector2 direction = Vector2.zero;
     Vector3 originPoint = Vector3.zero;
@@ -30,6 +37,7 @@ public class SneakerEnemy : BaseEnemy
         rigi = GetComponent<Rigidbody2D>();
         attackRadius = GetComponentInChildren<IsPlayerInTrigger>();
         currentTargetPoint = RandomTargetPoint();
+        animator = GetComponent<Animator>();
     }
 
     public override void Move()
@@ -45,20 +53,22 @@ public class SneakerEnemy : BaseEnemy
             }
 
 
-            
+
             currentTargetPoint = _Player.transform.position;
             direction = (currentTargetPoint - transform.position).normalized;
-
+            if (distanceToPlayer <= hitRange)
+            {
+                return; // is close enought to attack no need to get Closer
+            }
 
             if (!IsBeingLookedAt())
             {
-                transform.position += ((Vector3)direction * speed * Time.deltaTime)* sneakSpeedFactor;
+                transform.position += ((Vector3)direction * speed * Time.deltaTime) * sneakSpeedFactor;
             }
 
             else
             {
-
-                transform.position += ((Vector3)direction * speed  * Time.deltaTime) * lookedAtSpeedFactor;
+                transform.position += ((Vector3)direction * speed * Time.deltaTime) * lookedAtSpeedFactor;
             }
 
         }
@@ -77,15 +87,30 @@ public class SneakerEnemy : BaseEnemy
 
     }
 
-
+    IEnumerator Hit()
+    {
+        yield return new WaitForSeconds(ATTACKANIMATION_HIT_OFFSET);
+        if (distanceToPlayer <= hitRange)
+        { 
+            _Player.health.TakeDamage((int)damage); 
+        }
+    }
 
     public override void Attack()
     {
         if (distanceToPlayer <= hitRange && attackTimer >= attackCooldown)
         {
             //attack
+            StartCoroutine(Hit());
+            animator.SetTrigger("Attack");
             attackTimer = 0;
-            _Player.health.TakeDamage((int)damage);
+
+        }
+        if (distanceToPlayer <= contactRange && Time.deltaTime >= contactTimer)
+        {
+            contactTimer = Time.deltaTime + contactCooldown;
+
+            _Player.health.TakeDamage((int)contactDamage);
         }
     }
 
@@ -100,7 +125,7 @@ public class SneakerEnemy : BaseEnemy
     {
         if (_Player.transform != null)
         {
-            distanceToPlayer = (_Player.transform.position - this.transform.position).sqrMagnitude;
+            distanceToPlayer = Vector2.Distance(_Player.transform.position, transform.position);
         }
         attackTimer += Time.deltaTime;
         base.Update();
@@ -137,6 +162,12 @@ public class SneakerEnemy : BaseEnemy
             point.y < target.y + POINTMERCYAREA &&
             point.y > target.y - POINTMERCYAREA;
     }
+
+    public override void Hurt(float damage)
+    {
+        base.Hurt(damage);
+        animator.SetTrigger("Hurt");
+    }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
@@ -144,7 +175,7 @@ public class SneakerEnemy : BaseEnemy
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(currentTargetPoint, POINTMERCYAREA);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, transform.position+new Vector3(direction.x, direction.y));
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(direction.x, direction.y));
     }
 
 
--- Assets/Code/Scripts/Gameplay/PlayerCharacter/S_PlayerHealth.cs
+++ Assets/Code/Scripts/Gameplay/PlayerCharacter/S_PlayerHealth.cs
@@ -1,20 +1,22 @@
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class S_PlayerHealth : MonoBehaviour
 {
+    const float HIT_BLINK_DURATION = 0.1f;
     const int FIRST_LEVEL_BUILD_INDEX = 1;
     public int maxHealth = 100;
     private static int _CurrentHealth = 0;
     public int currentHealth;
-
-    public HealthUi healthUi;
+    [SerializeField] SpriteRenderer _Sprite;
 
 
     void Start()
     {
+        
         if(_CurrentHealth <= 0) 
         {
             _CurrentHealth = maxHealth;
@@ -22,14 +24,21 @@ public class S_PlayerHealth : MonoBehaviour
         currentHealth = _CurrentHealth;
     }
 
+    IEnumerator HitFeedBack() 
+    {
+        if(_Sprite != null)
+        {
+            _Sprite.color = Color.red;
+            yield return new WaitForSeconds(HIT_BLINK_DURATION);
+            _Sprite.color = Color.white;
+        }
+    }
+
     public void TakeDamage(int damageAmount)
     {
+        StartCoroutine(HitFeedBack());
         _CurrentHealth -= damageAmount;
         currentHealth = _CurrentHealth;
-        if (healthUi != null)
-        {
-            healthUi.GetDamage();
-        }
         if (_CurrentHealth <= 0)
         {
             Die();
