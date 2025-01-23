c023042ccf59fc5e052cc48480199642fd1cfae1
MrPowerpoint <tiziand4404@gmail.com>
2025-01-23 10:19:27 +0100

    Player Controller walljump fix and dynamic jump

--- Assets/Code/Scripts/LevelDesign/Hazards/Spikes.cs
+++ Assets/Code/Scripts/LevelDesign/Hazards/Spikes.cs
@@ -6,15 +6,16 @@ public class Spikes : MonoBehaviour
     private const string FIRST_LEVEL_NAME = "L1_Redo_Nika";
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        S_PlayerHealth.GetInstance().TakeDamage(5000);
         //Return to safepoint
-        if (Safepoint.GetCurrentSafepoint() != null)
-        {
-            SafepointObject.LoadCurrentSafepoint();
-        }
+        //if (Safepoint.GetCurrentSafepoint() != null)
+        //{
+        //    SafepointObject.LoadCurrentSafepoint();
+        //}
 
-        else
-        {
-            SceneManager.LoadScene(FIRST_LEVEL_NAME);
-        }
+        //else
+        //{
+        //    SceneManager.LoadScene(FIRST_LEVEL_NAME);
+        //}
     }
 }
