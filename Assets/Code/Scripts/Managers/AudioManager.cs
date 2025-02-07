ef49de6930b406f39489bd33217410640dd130fd
Oliver Grimm <olivergrimm89@gmail.com>
2025-02-07 10:07:21 +0100

    updated Prefab

--- Assets/Code/Scripts/Managers/AudioManager.cs
+++ Assets/Code/Scripts/Managers/AudioManager.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+
+    public static AudioManager instance;
+
+    [SerializeField] private AudioSource soundFXObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
     {
-        
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
+        float clipLength = audioSource.clip.length;
+        Destroy(audioSource.gameObject, clipLength);
     }
 }
