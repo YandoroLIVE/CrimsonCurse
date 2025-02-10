ba26add534af1afb6fe33bf81af498e276d70346
Oliver Grimm <olivergrimm89@gmail.com>
2025-02-10 17:02:50 +0100

    Audio

---
+++ Assets/MixerManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Audio;
+public class MixerManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetMasterVolume(float level)
+    {
+
+    }
+
+    public void SetSoundFXVolume(float level)
+    {
+
+    }
+    public void SetMusicVolume(float level)
+    {
+
+    }
+}
