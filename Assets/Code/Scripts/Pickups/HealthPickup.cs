294e94e61f0c49747516931629ac2ebb297da14f
Oliver Grimm <olivergrimm89@gmail.com>
2025-02-11 22:25:06 +0100

    health sounds

--- Assets/Code/Scripts/Pickups/HealthPickup.cs
+++ Assets/Code/Scripts/Pickups/HealthPickup.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class HealthPickup : MonoBehaviour
 {
     public int healthToRestore = 10;
+    [SerializeField] private AudioClip healthClip;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.name);
+        AudioManager.instance?.PlaySoundFXClip(healthClip, transform, 1f);
         S_PlayerHealth.GetInstance().Heal(healthToRestore);
         this.gameObject.SetActive(false);
     }
