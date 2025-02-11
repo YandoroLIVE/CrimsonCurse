d9d9257f08309872e9da9222a7d95b7be4a54cc7
MrPowerpoint <tiziand4404@gmail.com>
2025-02-11 13:26:32 +0100

    added ArtPickups and breakable walls

---
+++ Assets/Code/Scripts/LevelDesign/BreakableWall.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableWall : MonoBehaviour, IHurtable
+{
+    private const int BREAKABLE_WALL_AMOUNT = 5;
+    private static List<bool> brokenWalls = new List<bool>(BREAKABLE_WALL_AMOUNT);
+    [SerializeField, Range(0, BREAKABLE_WALL_AMOUNT - 1)] int wallID;
+    [SerializeField] private float health = 200;
+    [SerializeField] private ParticleSystem damageVFX;
+    void Start()
+    {
+        if (brokenWalls.Count == 0)
+        {
+            for (int i = 0; i < BREAKABLE_WALL_AMOUNT; ++i)
+            {
+                brokenWalls.Add(false);
+            }
+        }
+
+        else if (brokenWalls[wallID] == true) 
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void Hurt(float damage) 
+    {
+        health -= damage;
+        damageVFX?.Play();
+        if(health <= 0) 
+        {
+            brokenWalls[wallID] = true; 
+            damageVFX.transform.SetParent(null);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
