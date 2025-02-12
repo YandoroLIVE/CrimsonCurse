04d1ac64d01449c20c346aa8b567427d2288c825
MrPowerpoint <tiziand4404@gmail.com>
2025-02-12 11:49:18 +0100

    Fixed stuff qwq

--- Assets/Code/Scripts/UI/CooldownUI.cs
+++ Assets/Code/Scripts/UI/CooldownUI.cs
@@ -51,10 +51,16 @@ namespace HeroController
 
         private void GetReferences()
         {
-            player = FindObjectOfType<PlayerController>();
-            playerAttack = player.GetComponent<S_PlayerAttack>();
-            playerLongRangeAttack = player.GetComponent<S_PlayerLongRangeAttack>();
-            playerController = player.GetComponent<PlayerController>();
+            if(S_PlayerHealth.GetInstance() != null)
+            {
+                player = S_PlayerHealth.GetInstance().GetComponent<PlayerController>();
+                if (player != null)
+                {
+                    playerAttack = player.GetComponent<S_PlayerAttack>();
+                    playerLongRangeAttack = player.GetComponent<S_PlayerLongRangeAttack>();
+                    playerController = player.GetComponent<PlayerController>();
+                }
+            }
         }
     }
 
